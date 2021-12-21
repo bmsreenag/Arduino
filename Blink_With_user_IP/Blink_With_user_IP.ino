0a93a87fa22f2cb1575edbb500423317d0a45cfe
B.M.Sreenag <70892682+bmsreenag@users.noreply.github.com>
2021-12-21 20:51:45 +0530

    Update Blink_With_user_IP.ino

--- Blink_With_user_IP/Blink_With_user_IP.ino
+++ Blink_With_user_IP/Blink_With_user_IP.ino
@@ -4,21 +4,19 @@ int i;
 
 int delay_milliseconds = delay_seconds* 1000;
 float voltage = 2.5;
+float V_manual=(256/5)*voltage;
+float V_map = map(voltage,0,5,0,255);
+  
 
 void setup()
 {
   Serial.begin(9600);
-  
-  float V_manual = (256/5)*voltage;
-  float V_map = map(voltage,0,5,0,255);
-  
   Serial.println(V_manual);
-  Serial.println(V_map); 
+  Serial.println(V_map);
   pinMode(LEDpin , OUTPUT);
 }
 void loop()
 {
-  float V_manual = (256/5)*voltage;
   for(i=0;i<V_manual;i++)
   {
   voltage = i;
